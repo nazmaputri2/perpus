59455484ef5ef22fe2412d11092f0b5efad80b5a
RafifRafif <rafifruhulhaqq@gmail.com>
2025-07-06 14:23:38 +0700

    Menambahkan link/template excel pada import data siswa

--- resources/views/components/datasiswa/modal-import-siswa.blade.php
+++ resources/views/components/datasiswa/modal-import-siswa.blade.php
@@ -18,20 +18,28 @@
                 <div class="mb-4">
                     <p class="text-sm text-gray-600 mb-2">
                         Pastikan file Anda sesuai dengan format yang kami sediakan.
+                        <br>
+                        <a href="{{ asset('template/datasiswa.xlsx') }}"
+                            class="text-blue-600 hover:underline inline-flex items-center mt-1" download>
+                            <i class="fas fa-file-excel me-1"></i> Unduh Template Excel
+                        </a>
                     </p>
-                   
                 </div>
 
+
                 <div class="mb-6">
-                    <label for="file_import" class="block mb-2 text-sm font-medium text-gray-900">Pilih File untuk Diunggah</label>
+                    <label for="file_import" class="block mb-2 text-sm font-medium text-gray-900">Pilih File untuk
+                        Diunggah</label>
                     <input type="file" name="file" id="file_import"
                         class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none file:bg-gray-200 file:text-gray-800 file:p-2.5 file:border-0"
                         required accept=".xls,.xlsx">
-                    <p class="mt-1 text-xs text-gray-500" id="file_input_help">Hanya file .XLS atau .XLSX yang diterima.</p>
+                    <p class="mt-1 text-xs text-gray-500" id="file_input_help">Hanya file .XLS atau .XLSX yang diterima.
+                    </p>
                 </div>
 
                 <div class="flex items-center justify-end pt-4 border-t">
-                     <button data-modal-hide="importDataModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 me-3">
+                    <button data-modal-hide="importDataModal" type="button"
+                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 me-3">
                         Batal
                     </button>
                     <button type="submit"
